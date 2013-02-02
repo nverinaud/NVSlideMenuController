a86756ba5ef2b8870435afd493b957bc00e84ea6
Nicolas VERINAUD <n.verinaud@gmail.com>
2013-02-02 17:36:49 +0100

    Create Storyboard demo.

---
+++ Demos/Storyboard/StoryboardDemo/StoryboardDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoryboardDemo
+//
+//  Created by Nicolas VERINAUD on 02/02/13.
+//  Copyright (c) 2013 Nicolas VERINAUD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Demos/Storyboard/StoryboardDemo/StoryboardDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoryboardDemo
+//
+//  Created by Nicolas VERINAUD on 02/02/13.
+//  Copyright (c) 2013 Nicolas VERINAUD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
